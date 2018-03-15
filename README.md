4d1b342d19c2eececd1df7905b681fb1be8616c7
Daniel Schmitz <danieljfa@gmail.com>
2018-03-15 10:10:56 -0300

    remove all files

--- README.md
+++
@@ -1,7 +0,0 @@
-# Blog vuejs-brasil.com.br
-
-Este é o blog Vuejs Brasil. Migração do Ghost para o Jekyll
-
-Acesso preliminar: https://vuejs-br.github.io/blog/
-
-Tema baeado em https://github.com/willianjusten/cards-jekyll-template
--- src/js/target_blank.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var links = document.links;
-  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-    if (links[i].hostname != window.location.hostname) {
-      links[i].target = '_blank';
-      links[i].className += ' externalLink';
-    }
-  }
-})();
